c35be2bc46eb44b38c1628fdf1f5c297f5512cbd
Dasol0303 <ekthf9135@gmail.com>
2025-07-17 17:10:34 +0900

    비즈니스 요구사항 별 프로젝트 구조 및 파일 생성 연습

---
+++ src/main/java/com/app/controller/accomodation/AccomodationController.java
@@ -0,0 +1,13 @@
+package com.app.controller.accomodation;
+
+public class AccomodationController {
+
+	//업장
+	//업장별로 영업일
+	//영업호실세팅
+	//각 날짜별 비용 세팅
+	
+	//업장!
+	//업장관리자들이~ 접속 ~
+	//업장운영관련된 처리	! Controller
+}
---
+++ src/main/java/com/app/dto/businessday/BusinessDay.java
@@ -0,0 +1,9 @@
+package com.app.dto.businessday;
+
+public class BusinessDay {
+
+	//영업일
+	//공휴일 여부
+	//날짜
+	
+}
